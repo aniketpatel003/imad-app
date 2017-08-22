d29a5c50da7e17d554df454bc4eec68333687783
aniketpatel003 <31178209+aniketpatel003@users.noreply.github.com>
2017-08-22 15:09:51 +0100

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,7 @@ app.get('/article-one', function(req,res){
 })
 var counter=0;
 app.get('/counter', function(req,res){
+   counter=counter+1;
     res.send(counter.toString());
 });
 app.get('/article-two', function(req,res){
