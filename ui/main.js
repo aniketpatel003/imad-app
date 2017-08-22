43eafda5e3be2877b6019bfc654ed000e9c4970f
aniketpatel003 <31178209+aniketpatel003@users.noreply.github.com>
2017-08-22 15:36:57 +0100

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,7 +2,17 @@ var button= document.getElementById('counter');
 var counter=0;
 button.onclick= function(){
   
-  counter=counter+1;
-  var span=document.getElementById('count');
-  span.innerhtml=counter.toString();
+  var request= new XMLHttpRequest();
+  
+  request.onreadystatechange= function(){
+      if(request.readystate === XMLHttpRequest.DONE){
+          if(request.status=200){
+              var counter=request.responseText
+              var span= document.getElementById('count');
+              span.innerhtml= counter.toString();
+          }
+      }
+  };
+  request.open('GET', 'http://aniketpatel1997.imad.hasura-app.io/counter', true);
+  request.send(null);
 };
